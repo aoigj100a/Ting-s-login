d3c865847f644e2dd50fc1bd761bc6c7e67b68ff
aoigj100a <aoigj100@gmail.com>
2021-01-31 19:59:24 +0800

    feat:用cookie 保存使用者登入狀態

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cookieParser = require('cookie-parser')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
@@ -15,25 +16,49 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 //使用bodyParser轉碼 以取得表單資料
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 
-   app.get('/',(req,res)=>{
-        res.render('index')
-   })
-   app.post('/login',(req,res)=>{
-    //    console.log(`帳號：${req.body.email},密碼：${req.body.password}`)
-      const result = checkForm(req.body)
-      if(result.includes('歡迎')){
-        res.send(`${result} <a href="/logout"><p>登出</p></a>`)
-        
-      }else{
-        res.send(`${result}`)
-      }
-   })
+app.get('/', (req, res) => {
+  console.log(req.cookies.username)
+  if (!req.cookies.username) {
+    res.render('index')
+  } else {
+    res.redirect('/login')
+  }
 
-   app.get('/logout', (req, res) => {
+})
+
+app.get('/login',(req,res)=>{
+  if (!req.cookies.username) {
     res.redirect('/')
-  })
+  } else {
+    res.send(`${req.cookies.username}登入成功 <a href="/logout"><p>登出</p></a>`)
+  }
+})
 
-   app.listen(port,()=>{
-       console.log(`已經連線到http://localhost:${port}`)
-   })
+app.post('/login', (req, res) => {
+  // console.log(`帳號：${req.body.email},密碼：${req.body.password}`)
+
+    const result = checkForm(req.body)
+    if (result.includes('歡迎')) {
+      //登入成功之後設置cookie
+      res.cookie('username', req.body.email, { maxAge: 900000, httpOnly: true })
+      res.send(`${result} <a href="/logout"><p>登出</p></a>`)
+
+    } else {
+      res.send(`${result}`)
+    }
+  } 
+)
+
+
+
+app.get('/logout', (req, res) => {
+  //登出時刪除Cookie
+  res.clearCookie('username')
+  res.redirect('/')
+})
+
+app.listen(port, () => {
+  console.log(`已經連線到http://localhost:${port}`)
+})
